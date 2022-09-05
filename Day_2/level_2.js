2ba7a9dc28417f10b472ed4c185004f5b9c2b011
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-09-05 20:16:14 +0100

    feat: Day_2 level_2

---
+++ Day_2/level_2.js
@@ -0,0 +1,26 @@
+console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.')
+
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+
+console.log(typeof(parseInt('10')) == typeof(10))
+
+console.log(Math.round(parseFloat('9.8')) == 10)
+
+let words = 'python jargon';
+console.log(words.includes('on'))
+
+let sentence = 'I hope this course is not full of jargon';
+console.log(sentence.includes('jargon'))
+
+console.log(Math.floor(Math.random() * 101))
+
+console.log(Math.floor(Math.random() * (101 - 50) + 50))
+
+console.log(Math.floor(Math.random() * 256))
+
+let myStr = 'JavaScript';
+console.log(myStr.charAt(Math.floor(Math.random() * 10)))
+
+console.log("1 1 1 1 1 \n2 1 2 4 8 \n3 1 3 9 27 \n4 1 4 16 64 \n5 1 5 25 125")
+
+console.log(newString.substr(31, 23))

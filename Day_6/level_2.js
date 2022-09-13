656c5eab47d0b248b9c936a774560e8eca05ca3d
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-09-13 23:16:28 +0100

    feat: Day_6 level_2

--- Day_6/level_2.js
+++ Day_6/level_2.js
@@ -19,7 +19,7 @@ for(let i = 1; i <= 6; i++){
 console.log(hexadecimalValue)
 
 
-// // Question 3
+// Question 3
 // let randValue = "01234567890";
 // let bracket = ',)'
 // let rgb = "rgb(";
@@ -28,3 +28,36 @@ console.log(hexadecimalValue)
 //     rgb += randomNumber + bracket
 // }
 // console.log(rgb)
+
+
+
+// Question 4
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+let newCountries = []
+for(let i = 0; i < countries.length; i++){
+    newCountries.push(countries[i].toUpperCase())
+}
+console.log(newCountries)
+
+
+// Question 5
+let countriesLength = []
+for(let country of countries){
+    countriesLength.push(country.length)
+}
+console.log(countriesLength)
+
+
+// Question 6

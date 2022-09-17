cdb640425d984f943f6756d581cd7c5e25396b30
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-09-17 23:30:02 +0100

    feat: Day_6 level_3

---
+++ Day_6/level_3.js
@@ -0,0 +1,24 @@
+// Question 1
+const unsortedCountries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ];
+
+
+// Question 2
+let sortedCountries = unsortedCountries.sort().slice(0, unsortedCountries.length);
+console.log(sortedCountries)
+
+
+// Question 3
+let sortedWebTechs = webTechs.sort();
+let sortedMernStack = mernStack.sort();

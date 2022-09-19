ccb3a169d23d6cadee58432d82212cb642988a06
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-09-19 23:10:01 +0100

    feat: Day_6 level_3

--- Day_6/level_3.js
+++ Day_6/level_3.js
@@ -22,3 +22,21 @@ console.log(sortedCountries)
 // Question 3
 let sortedWebTechs = webTechs.sort();
 let sortedMernStack = mernStack.sort();
+
+
+// Question 4
+let landCountries = [];
+for(let country of countries){
+  if(country.includes('land')){
+    landCountries.push(country)
+  }
+}
+console.log(landCountries)
+
+
+// Question 5
+let maxNumLength;
+for(let i = 0; i < countries.length; i++){
+  
+}
+console.log(maxNumLength)

4be8372b8baea4c0f3ec7c314e2eebd0a1ee64a8
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-09-19 23:51:35 +0100

    feat: Day_6 level_3

--- Day_6/level_3.js
+++ Day_6/level_3.js
@@ -35,8 +35,40 @@ console.log(landCountries)
 
 
 // Question 5
-let maxNumLength;
-for(let i = 0; i < countries.length; i++){
-  
+// let maxCountryLength = 0;
+// for(let country of countries){
+//     maxCountryLength = country.length
+// }
+// console.log(maxCountryLength)
+
+
+// Question 6
+// Refer to question 4
+
+
+// Question 7
+let fourCharCountry = [];
+for(let country of countries){
+  if(country.length == 4){
+    fourCharCountry.push(country)
+  }
 }
-console.log(maxNumLength)
+console.log(fourCharCountry)
+
+
+// Question 8
+let twoChar = [];
+for(let country of countries){
+  if(country.length >= 2){
+    twoChar.push(country)
+  }
+}
+console.log(twoChar)
+
+
+// Question 9
+const reversedCountries = [];
+for (let i = countries.length - 1; i >= 0; i--) {
+  reversedCountries.push(countries[i].toUpperCase());
+}
+console.log(reversedCountries);~

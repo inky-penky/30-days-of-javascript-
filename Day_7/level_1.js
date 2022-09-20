9327f22b927690021c4d2ae88cb9943231327af5
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-09-20 23:52:43 +0100

    feat: Day_7 level 1

---
+++ Day_7/level_1.js
@@ -0,0 +1,35 @@
+// Question 1
+function fullName(){
+    let firstName = 'Ahmad'
+    let lastName = 'Abdulazeez'
+    let fullName = firstName + ' ' + lastName
+    return fullName;
+}
+console.log(fullName())
+
+
+// Question 2
+function myFullName(firstName, lastName){
+    let fullName = firstName + ' ' + lastName
+    return fullName;
+}
+console.log(myFullName('Ahmad', 'Abdulazeez'))
+
+
+// Question 3
+function addNumbers(a, b) {
+    let sum = a + b;
+    return sum;
+}
+console.log(addNumbers(2, 3))
+
+
+// Question 4
+function areaOfRectangle(length, width){
+    let area = length * width
+    return area;
+}
+console.log(areaOfRectangle(3, 4))
+
+
+// Question 5

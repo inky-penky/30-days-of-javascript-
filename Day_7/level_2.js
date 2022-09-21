b5964486462e6032a44586cddc93adb1325cbe1a
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-09-21 23:51:46 +0100

    feat: Day_7 level 2

---
+++ Day_7/level_2.js
@@ -0,0 +1,12 @@
+// Question 3
+function printArray(array) {
+    let arr = [];
+    for(let i = 0; i < array.length; i++){
+        arr.push(array[i]) 
+    }
+    return arr;
+}
+console.log(printArray([1, 'we', 'see', 6, true]))
+
+
+// Question 4

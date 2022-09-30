6dc5cff643f1caedab1258f3737069b02ed7bd5b
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-09-30 20:31:49 +0100

    feat: Day_7 level_3

--- Day_7/level_3.js
+++ Day_7/level_3.js
@@ -1,14 +1,13 @@
 // Question 1
-function userIdGenerator() {
+function userIdGenerator(numOfChar, numOfId) {
     let userIdChar = "0123456789abcdefghijklmnopqrstuvwxyz";
     let userId = '';
-    for(let i = 0; i <= 6; i++){
+    numOfChar = prompt('Input number of character to be generated')
+    numOfId = prompt('Input the number of Ids to be generated')
+    for(let i = 1; i <= numOfChar; i++){
         let randomNum = Math.floor(Math.random() * userIdChar.length)
         userId += userIdChar[randomNum]
     }
     return userId.toUpperCase();
 }
-function userIdGeneratedByUser(numOfChar, numOfId) {
-    numOfChar = prompt('Input number of character to be generated')
-    numOfId = prompt('Input the number of Ids to be generated')
-}
+console.log(userIdGenerator())
